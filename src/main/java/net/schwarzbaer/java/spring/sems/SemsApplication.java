0538d82925e00b73ace1288fc552ff9110e7d690
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-06-10 15:16:37 +0200

    repo created from generated project

---
+++ src/main/java/net/schwarzbaer/java/spring/sems/SemsApplication.java
@@ -0,0 +1,13 @@
+package net.schwarzbaer.java.spring.sems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/schwarzbaer/java/spring/sems/SemsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schwarzbaer.java.spring.sems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
