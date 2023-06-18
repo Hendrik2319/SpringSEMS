87d6b2de2e652ed92ed87426097bf083c658de8d
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-06-18 19:09:50 +0200

    inmemory user management replaced by LDAP server

--- src/main/java/net/schwarzbaer/java/spring/sems/WebSecurityConfig.java
+++ src/main/java/net/schwarzbaer/java/spring/sems/WebSecurityConfig.java
@@ -1,17 +1,14 @@
 package net.schwarzbaer.java.spring.sems;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
     
 	@Bean
@@ -35,6 +32,21 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                    .url("ldap://localhost:8389/dc=springframework,dc=org")
+                    .and()
+                .passwordCompare()
+                    .passwordEncoder(new BCryptPasswordEncoder())
+                    .passwordAttribute("userPassword");
+    }
+
+	/*
 	@Bean
 	public UserDetailsService userDetailsService() {
 		UserDetails user =
@@ -46,4 +58,5 @@ public class WebSecurityConfig {
 
 		return new InMemoryUserDetailsManager(user);
 	}    
+	 */
 }
