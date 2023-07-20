31f51bdacf7091544c16b282624c04628609b23e
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-07-20 22:42:20 +0200

    feat: some tests added springframework version changed to 3.1.2

--- src/test/java/net/schwarzbaer/java/spring/sems/SemsApplicationTests.java
+++ src/test/java/net/schwarzbaer/java/spring/sems/SemsApplicationTests.java
@@ -1,13 +1,32 @@
 package net.schwarzbaer.java.spring.sems;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import net.schwarzbaer.java.spring.sems.address.AddressController;
+import net.schwarzbaer.java.spring.sems.employee.EmployeeController;
+import net.schwarzbaer.java.spring.sems.department.DepartmentController;
+import net.schwarzbaer.java.spring.sems.ldap.LdapInspectController;
+
+//@SuppressWarnings("unused")
 @SpringBootTest
 class SemsApplicationTests {
 
+	@Autowired private LdapInspectController ldapInspectController;
+	@Autowired private DepartmentController departmentController;
+	@Autowired private EmployeeController employeeController;
+	@Autowired private AddressController addressController;
+	@Autowired private UserInterface userInterface;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(ldapInspectController).isNotNull();
+		Assertions.assertThat(departmentController).isNotNull();
+		Assertions.assertThat(employeeController).isNotNull();
+		Assertions.assertThat(addressController).isNotNull();
+		Assertions.assertThat(userInterface).isNotNull();
 	}
 
 }
